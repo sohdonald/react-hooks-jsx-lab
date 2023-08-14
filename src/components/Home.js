47ff29196359a3ea06c39cc0368712d4400a8757
Donald Soh <sohdonald@gmail.com>
2023-08-14 16:45:35 -0400

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const homeColor = {
+  color:"firebrick"
+}
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+
+  return (
+  <div id="home">
+    Home
+  <h1 style={homeColor}>{name} is a Web Developer from {city}</h1>
+  </div>
+  );
 }
 
 export default Home;
